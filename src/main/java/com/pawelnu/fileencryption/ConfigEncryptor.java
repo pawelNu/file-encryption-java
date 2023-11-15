68f2ccff4c1405b7bbf47a91fae03a2c63045dc0
pawelNu <pnurzynski17@gmail.com>
2023-11-15 20:39:00 +0100

    feat (1): create ConfigEncryptor with key generation and encryption

---
+++ src/main/java/com/pawelnu/fileencryption/ConfigEncryptor.java
@@ -0,0 +1,37 @@
+package com.pawelnu.fileencryption;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.Security;
+import java.util.Arrays;
+import java.util.Base64;
+
+public class ConfigEncryptor {
+
+    private static final String ALGORITHM = "AES";
+    private static final String TRANSFORMATION = "AES";
+    private static final String PROVIDER = "BC"; // Bouncy Castle provider
+
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    private static SecretKey generateKey(String password) throws Exception {
+        MessageDigest sha = MessageDigest.getInstance("SHA-256");
+        byte[] keyBytes = sha.digest(password.getBytes(StandardCharsets.UTF_8));
+        return new SecretKeySpec(Arrays.copyOf(keyBytes, 32), ALGORITHM); // 32 bytes = 256 bits
+    }
+
+    public static String encrypt(String input, String password) throws Exception {
+        Cipher cipher = Cipher.getInstance(TRANSFORMATION, PROVIDER);
+        cipher.init(Cipher.ENCRYPT_MODE, generateKey(password));
+        byte[] encryptedBytes = cipher.doFinal(input.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+}
