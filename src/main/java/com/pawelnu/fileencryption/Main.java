4fac5160fcc99531215a42a641a6a5cc9ece6147
pawelNu <93542936+pawelNu@users.noreply.github.com>
2023-11-16 19:25:11 +0100

    Feat 11 create console interface for encrypting file (#12)
    * refactor (11): deleted unnecessary class
    
    * feat (11): created application console interface
    
    * feat (11): added to Main

--- src/main/java/com/pawelnu/fileencryption/Main.java
+++ src/main/java/com/pawelnu/fileencryption/Main.java
@@ -1,11 +1,13 @@
 package com.pawelnu.fileencryption;
 
-import com.pawelnu.fileencryption.logic.FileLogic;
+import com.pawelnu.fileencryption.console.ConsoleInterface;
+
 
 public class Main {
     public static void main(String[] args) {
 
-        FileLogic.main(args);
+//        ApplicationLogic.main(args);
 
+        ConsoleInterface.applicationInterface();
     }
 }
