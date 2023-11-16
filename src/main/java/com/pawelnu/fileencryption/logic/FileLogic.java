2c2b8c6eeed62afb5659b88c9b553d34514b669e
pawelNu <pnurzynski17@gmail.com>
2023-11-16 18:17:51 +0100

    feat (7): added reading encrypted config file to application logic

--- src/main/java/com/pawelnu/fileencryption/logic/FileLogic.java
+++ src/main/java/com/pawelnu/fileencryption/logic/FileLogic.java
@@ -6,24 +6,41 @@ import java.io.IOException;
 
 public class FileLogic {
     public static void main(String[] args) {
+
+        // reading the encrypted file
+//        try {
+//            ConfigParser configParser = new ConfigParser();
+//            configParser.loadConfig("config.properties");
+//
+//            String valueTest = configParser.getConfigValue("test.test");
+//            String valueTest2 = configParser.getConfigValue("test.test2");
+//
+//            System.out.println("test.test: " + valueTest);
+//            System.out.println("test.test2: " + valueTest2);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+
+        // file encryption
+//        try {
+//            ConfigParser configParser = new ConfigParser();
+//            configParser.loadConfigEncryptAndSaveToFile("config.properties");
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+
+        // reading the encrypted file
         try {
             ConfigParser configParser = new ConfigParser();
-            configParser.loadConfig("config.properties");
+            configParser.loadEncryptedConfig("config.properties.encrypted");
 
             String valueTest = configParser.getConfigValue("test.test");
             String valueTest2 = configParser.getConfigValue("test.test2");
 
             System.out.println("test.test: " + valueTest);
             System.out.println("test.test2: " + valueTest2);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            ConfigParser configParser = new ConfigParser();
-            configParser.loadConfigEncryptAndSaveToFile("config.properties");
-
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
