2b586784d3f7eb117ca121994cbeb273074e8e06
pawelNu <93542936+pawelNu@users.noreply.github.com>
2023-11-18 14:41:26 +0100

    Feat 17 create documentation (#18)
    * refactor (17): code refactor
    
    * docs (17): created application documentation

--- src/main/java/com/pawelnu/fileencryption/Main.java
+++ src/main/java/com/pawelnu/fileencryption/Main.java
@@ -6,8 +6,7 @@ import com.pawelnu.fileencryption.console.ConsoleInterface;
 public class Main {
     public static void main(String[] args) {
 
-//        FileLogic.main(args);
-
         ConsoleInterface.applicationInterface();
+
     }
 }
